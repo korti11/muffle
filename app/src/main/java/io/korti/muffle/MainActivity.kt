2f3300996eb02b0b8f2497f3c002ff1248608e08
Korti <lucas.korthauer@gmail.com>
2019-12-04 14:24:12 +0100

    Added menu item for syncing with firebase. Currently showing toast with which menu item was pressed.

--- app/src/main/java/io/korti/muffle/MainActivity.kt
+++ app/src/main/java/io/korti/muffle/MainActivity.kt
@@ -49,7 +49,14 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_sync -> {
+                Toast.makeText(this, "Sync with firebase.", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_settings -> {
+                Toast.makeText(this, "Open SettingsActivity.", Toast.LENGTH_SHORT).show()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
