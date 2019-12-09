a0524535aeeb7b2e26784bc3edb3e2b1bbb5792e
Korti <lucas.korthauer@gmail.com>
2019-12-09 16:09:46 +0100

    Added UI tests for the EditMufflePointActivity. #4 ready for testing.

---
+++ app/src/androidTest/java/io/korti/muffle/EditMufflePointActivityEspressoTest.kt
@@ -0,0 +1,61 @@
+package io.korti.muffle
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import io.korti.muffle.adapter.MuffleCardAdapter
+import org.hamcrest.Matchers.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class EditMufflePointActivityEspressoTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun onBackButtonPress() {
+        onView(withId(R.id.muffleCards)).
+            perform(scrollToPosition<MuffleCardAdapter.MuffleCardHolder>(1))
+        onView(withChild(withText("Home"))).check(matches(isDisplayed()))
+        onView(allOf(withId(R.id.editButton), isDescendantOfA(withChild(withText("Home")))))
+            .perform(ViewActions.click())
+        onView(withText(R.string.title_activity_edit_muffle_point))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.muffleName))
+            .check(matches(withText("Home")))
+        onView(withContentDescription("Navigate up"))
+            .perform(ViewActions.click())
+        onView(withText(R.string.app_name))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun onSaveButtonPress() {
+        onView(withId(R.id.muffleCards)).
+            perform(scrollToPosition<MuffleCardAdapter.MuffleCardHolder>(1))
+        onView(withChild(withText("Home"))).check(matches(isDisplayed()))
+        onView(allOf(withId(R.id.editButton), isDescendantOfA(withChild(withText("Home")))))
+            .perform(ViewActions.click())
+        onView(withText(R.string.title_activity_edit_muffle_point))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.muffleName))
+            .check(matches(withText("Home")))
+        onView(withId(R.id.action_save)).perform(ViewActions.click())
+        onView(withText("Edited muffle point saved."))
+            .inRoot(withDecorView(not(`is`(activityRule.activity.window.decorView))))
+            .check(matches(isDisplayed()))
+        onView(withText(R.string.app_name))
+            .check(matches(isDisplayed()))
+    }
+
+}
