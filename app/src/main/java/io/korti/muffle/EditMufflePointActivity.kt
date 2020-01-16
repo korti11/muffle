ae65be96fca88cf6b5fc1d42a4b8bb503de317c6
Korti <lucas.korthauer@gmail.com>
2020-01-16 12:58:08 +0100

    Changed the image view to a google maps view.

--- app/src/main/java/io/korti/muffle/EditMufflePointActivity.kt
+++ app/src/main/java/io/korti/muffle/EditMufflePointActivity.kt
@@ -1,22 +1,23 @@
 package io.korti.muffle
 
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Base64
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.SeekBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
-import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import io.korti.muffle.audio.AudioManager
 import io.korti.muffle.database.entity.MufflePoint
 import io.korti.muffle.viewmodel.EditMufflePointActivityViewModel
 import kotlinx.android.synthetic.main.activity_edit_muffle_point.*
 import kotlinx.android.synthetic.main.content_edit_muffle_point.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class EditMufflePointActivity : AppCompatActivity() {
@@ -50,33 +51,87 @@ class EditMufflePointActivity : AppCompatActivity() {
             audioManager.getMaxVolumeOfPhone(android.media.AudioManager.STREAM_NOTIFICATION)
         alarmVolume.max = audioManager.getMaxVolumeOfPhone(android.media.AudioManager.STREAM_ALARM)
 
-        editMufflePointActivityViewModel.mufflePoint.observe(this, Observer {
-            lifecycleScope.launch {
-                withContext(Dispatchers.Default) {
-                    val imageAsByteArray = Base64.decode(it.image, Base64.DEFAULT)
-                    val bitmap =
-                        BitmapFactory.decodeByteArray(imageAsByteArray, 0, imageAsByteArray.size)
-                    withContext(Dispatchers.Main) {
-                        mapsImage.setImageBitmap(bitmap)
-                    }
-                }
+        muffleRange.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            /**
+             * Notification that the progress level has changed. Clients can use the fromUser parameter
+             * to distinguish user-initiated changes from those that occurred programmatically.
+             *
+             * @param seekBar The SeekBar whose progress has changed
+             * @param progress The current progress level. This will be in the range min..max where min
+             * and max were set by [ProgressBar.setMin] and
+             * [ProgressBar.setMax], respectively. (The default values for
+             * min is 0 and max is 100.)
+             * @param fromUser True if the progress change was initiated by the user.
+             */
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                editMufflePointActivityViewModel.updateRadius(progress)
+                rangeLabel.text = seekBar?.context?.getString(R.string.label_range, progress + 100)
             }
-            muffleName.setText(it.name)
-            rangeLabel.text = applicationContext.getString(R.string.label_range, it.radius.toInt())
-            enableSwitch.isChecked = it.status >= MufflePoint.Status.ENABLE
-            ringtoneCheckBox.isChecked = it.ringtoneVolume >= 0
-            ringtoneVolume.progress = if (ringtoneCheckBox.isChecked) it.ringtoneVolume else 0
-            mediaCheckBox.isChecked = it.mediaVolume >= 0
-            mediaVolume.progress = if (mediaCheckBox.isChecked) it.mediaVolume else 0
-            notificationsCheckBox.isChecked = it.notificationVolume >= 0
-            notificationsVolume.progress =
-                if (notificationsCheckBox.isChecked) it.notificationVolume else 0
-            alarmCheckBox.isChecked = it.alarmVolume >= 0
-            alarmVolume.progress = if (alarmCheckBox.isChecked) it.alarmVolume else 0
+
+            /**
+             * Notification that the user has started a touch gesture. Clients may want to use this
+             * to disable advancing the seekbar.
+             * @param seekBar The SeekBar in which the touch gesture began
+             */
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Don't need this.
+            }
+
+            /**
+             * Notification that the user has finished a touch gesture. Clients may want to use this
+             * to re-enable advancing the seekbar.
+             * @param seekBar The SeekBar in which the touch gesture began
+             */
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Don't need this.
+            }
+
         })
 
-        val mufflePointId = intent.extras?.getString(MUFFLE_POINT_EXTRA)
-        editMufflePointActivityViewModel.loadMufflePoint(mufflePointId.orEmpty())
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.googleMap) as SupportMapFragment
+
+        mapFragment.getMapAsync { map ->
+            editMufflePointActivityViewModel.mufflePoint.observe(this, Observer {
+                editMufflePointActivityViewModel.mapCircle.value = map.addCircle(
+                    CircleOptions().center(
+                        LatLng(it.lat, it.lng)
+                    ).radius(it.radius).strokeColor(
+                        ContextCompat.getColor(
+                            applicationContext,
+                            R.color.colorAccent
+                        )
+                    )
+                )
+                editMufflePointActivityViewModel.mapZoom.observe(this, Observer { zoom ->
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(it.lat, it.lng), zoom))
+                })
+                map.addMarker(MarkerOptions().position(LatLng(it.lat, it.lng)))
+                map.moveCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                        LatLng(it.lat, it.lng),
+                        editMufflePointActivityViewModel.mapZoom.value!!
+                    )
+                )
+
+                muffleName.setText(it.name)
+                rangeLabel.text =
+                    applicationContext.getString(R.string.label_range, it.radius.toInt())
+                enableSwitch.isChecked = it.status >= MufflePoint.Status.ENABLE
+                ringtoneCheckBox.isChecked = it.ringtoneVolume >= 0
+                ringtoneVolume.progress = if (ringtoneCheckBox.isChecked) it.ringtoneVolume else 0
+                mediaCheckBox.isChecked = it.mediaVolume >= 0
+                mediaVolume.progress = if (mediaCheckBox.isChecked) it.mediaVolume else 0
+                notificationsCheckBox.isChecked = it.notificationVolume >= 0
+                notificationsVolume.progress =
+                    if (notificationsCheckBox.isChecked) it.notificationVolume else 0
+                alarmCheckBox.isChecked = it.alarmVolume >= 0
+                alarmVolume.progress = if (alarmCheckBox.isChecked) it.alarmVolume else 0
+            })
+
+            val mufflePointId = intent.extras?.getString(MUFFLE_POINT_EXTRA)
+            editMufflePointActivityViewModel.loadMufflePoint(mufflePointId.orEmpty())
+        }
     }
 
     /**
--- app/src/main/java/io/korti/muffle/viewmodel/EditMufflePointActivityViewModel.kt
+++ app/src/main/java/io/korti/muffle/viewmodel/EditMufflePointActivityViewModel.kt
@@ -1,29 +1,52 @@
 package io.korti.muffle.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.android.gms.maps.model.Circle
 import io.korti.muffle.database.dao.MufflePointDao
 import io.korti.muffle.database.entity.MufflePoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlin.math.atan
 
 class EditMufflePointActivityViewModel @Inject constructor(private val mufflePointDao: MufflePointDao) :
     ViewModel() {
 
+    companion object {
+        private val TAG = EditMufflePointActivityViewModel::class.java.simpleName
+    }
+
     val mufflePoint = MutableLiveData<MufflePoint>()
+    val mapCircle = MutableLiveData<Circle>()
+    val mapZoom = MutableLiveData<Float>()
 
     fun loadMufflePoint(mufflePointId: String) {
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
                 val mufflePoint = mufflePointDao.getById(mufflePointId)
                 withContext(Dispatchers.Main) {
+                    mapZoom.value = getZoomLevel(mufflePoint.radius.toInt())
                     this@EditMufflePointActivityViewModel.mufflePoint.value = mufflePoint
                 }
             }
         }
+    }
+
+    fun updateRadius(radius: Int) {
+        val newRadius = radius.toDouble() + 100
+        val zoomLevel = getZoomLevel(radius)
+        Log.d(TAG, "Current Radius: ${mapCircle.value?.radius}, Current Zoom: ${mapZoom.value}")
+        Log.d(TAG, "Radius: $newRadius, Zoom: $zoomLevel")
+        mapCircle.value?.radius = newRadius
+        mapZoom.value = zoomLevel
+    }
+
+    private fun getZoomLevel(radius: Int): Float {
+        return 15F - (2.201F * atan(radius / 400.0).toFloat())
     }
 
 }
